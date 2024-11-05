d51590804cd4605e0d4628370acadabab73a2c03
jason9075 <jason9075@users.noreply.github.com>
2024-11-06 00:43:23 +0800

    hotfix nixvim lsp error

--- home/nixvim/lsp.nix
+++ home/nixvim/lsp.nix
@@ -10,6 +10,7 @@
         dockerls = { enable = true; };
         gopls = { enable = true; };
         clangd = { enable = true; };
+        eslint = { enable = true; };
         html = { enable = true; };
         ts-ls = { enable = true; };
         yamlls = { enable = true; };
@@ -29,10 +30,55 @@
         };
       };
     };
-
+    lsp-format.enable = true;
+    none-ls = {
+      enable = true;
+      sources = {
+        formatting = {
+          gofmt.enable = true;
+          nixfmt.enable = true;
+          hclfmt.enable = true;
+          stylua.enable = true;
+          prettierd.enable = true;
+          yamlfmt.enable = true;
+          black.settings = {
+            enable = true;
+            withArgs = ''
+              {
+                extra_args = { "--fast" },
+              }
+            '';
+          };
+          clang_format.settings = {
+            enable = true;
+            withArgs = # lua
+              ''
+                {
+                  extra_args = { "--style={BasedOnStyle: Google, ColumnLimit: 120}" },
+                  extra_filetypes = { "glsl" },
+                }
+              '';
+          };
+        };
+        diagnostics = {
+          dotenv_linter.enable = true;
+          pylint.enable = true;
+          zsh.enable = true;
+        };
+      };
+      settings = {
+        diagnostic_config = {
+          virtual_text = false;
+          signs = true;
+          update_in_insert = false;
+          underline = true;
+          severity_sort = true;
+        };
+      };
+    };
+    otter.enable = true;
     cmp = {
       enable = true;
-      autoEnableSources = true;
       settings = {
         sources = [
           { name = "nvim_lsp"; }
@@ -54,78 +100,14 @@
     cmp-path.enable = true;
     cmp-treesitter.enable = true;
     cmp_luasnip.enable = true;
-
-    lsp-format = {
-      enable = true;
-      setup = { gofmt = { force = true; }; };
-    };
-    none-ls = {
-      enable = true;
-      enableLspFormat = false;
-      sources = {
-        formatting = {
-          gofmt.enable = true;
-          nixfmt.enable = true;
-          hclfmt.enable = true;
-          stylua.enable = true;
-          prettierd.enable = true;
-          yamlfmt.enable = true;
-          black.settings = {
-            enable = true;
-            withArgs = ''
-              {
-                extra_args = { "--fast" },
-              }
-            '';
-          };
-          clang_format.settings = {
-            enable = true;
-            withArgs = ''
-              {
-                extra_args = { "--style={BasedOnStyle: Google, ColumnLimit: 120}" },
-                extra_filetypes = { "glsl" },
-              }
-            '';
-          };
-        };
-        diagnostics = {
-          dotenv_linter.enable = true;
-          pylint.enable = true;
-        };
-      };
-
-      settings = {
-        diagnostic_config = {
-          virtual_text = true;
-          signs = true;
-          update_in_insert = false;
-          underline = true;
-          severity_sort = true;
-        };
-        on_attach = # lua
-          ''
-            function(client, bufnr)
-              local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
-              if client.supports_method("textDocument/formatting") then
-                vim.api.nvim_clear_autocmds({
-                  group = augroup,
-                  buffer = bufnr
-                })
-                vim.api.nvim_create_autocmd("BufWritePre", {
-                  group = augroup,
-                  buffer = bufnr,
-                  callback = function()
-                    vim.lsp.buf.format({ async = false })
-                  end
-                })
-              end
-            end
-          '';
-      };
-    };
     treesitter = {
       enable = true;
-      settings = { auto_install = true; };
+      nixGrammars = true;
+      settings = {
+        auto_install = true;
+        highlight.enable = true;
+        indent.enable = true;
+      };
     };
     treesitter-context.enable = true;
     treesitter-textobjects.enable = true;
