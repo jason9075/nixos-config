8133e49ac182e9d28c13fa6d75aa4d5536ebb4aa
jason9075 <jason9075@users.noreply.github.com>
2025-04-06 14:55:25 +0800

    update: disable nvim pylint too many local variables

--- home/nixvim_config/lsp.nix
+++ home/nixvim_config/lsp.nix
@@ -83,8 +83,10 @@
           pylint = {
             enable = true;
             settings = {
-              extra_args =
-                [ "--disable=C0114,C0115,C0116" ]; # disable docstring warnings
+              extra_args = [
+                "--disable=C0114,C0115,C0116" # disable docstring warnings
+                "--max-locals=35" # R0914 too many local variables warning
+              ];
             };
           };
           zsh.enable = true;
