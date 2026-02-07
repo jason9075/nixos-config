fecb83049a8c669d9442a83c0ebfa1fbad6d2d8f
jason9075 <jason9075@users.noreply.github.com>
2026-02-08 02:23:59 +0800

    update(clawbot): remove hardcode config and docs

--- home/services/openclaw.nix
+++ home/services/openclaw.nix
@@ -9,26 +9,6 @@ in {
 
   programs.openclaw = {
     enable = true;
-    # documents = ./openclaw/docs;
-
-    config = {
-      agents.defaults = {
-        model.primary = "openai/gpt-5.2";
-        models."openai/gpt-5.2".alias = "gpt";
-      };
-
-      messages.ackReactionScope = "group-mentions";
-
-      gateway = {
-        mode = "local";
-        port = 18789;
-        bind = "loopback";
-        auth = {
-          mode = "token";
-        };
-      };
-    };
-
     instances.default.enable = true;
   };
   
